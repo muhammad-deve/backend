921464a5991cc382af86cc930d13ffe3f0f4deb3
Muslimbek Yarashev <108422258+cullinanMy@users.noreply.github.com>
2025-02-03 17:24:11 +0500

    fixing bugs

---
+++ app/artifacts/migrations/1738581070_updated_user_book_rates.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2201851195")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE UNIQUE INDEX ` + "`" + `idx_wPM9QQIGTH` + "`" + ` ON ` + "`" + `user_book_rates` + "`" + ` (\n  ` + "`" + `user` + "`" + `,\n  ` + "`" + `book` + "`" + `\n)"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2201851195")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
---
+++ app/artifacts/migrations/1738585411_updated_users.go
@@ -0,0 +1,58 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(15, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_1909313007",
+			"hidden": false,
+			"id": "relation572895885",
+			"maxSelect": 999,
+			"minSelect": 0,
+			"name": "favourite_genre",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(15, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_1909313007",
+			"hidden": false,
+			"id": "relation572895885",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "favourite_genre",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
