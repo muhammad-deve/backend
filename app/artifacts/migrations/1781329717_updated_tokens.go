8458c0f87de1c8775b20b4821166597da3577e86
Muhammad <muhammadgo.deve@gmail.com>
2026-06-13 10:53:52 +0500

    Added api-rules

---
+++ app/artifacts/migrations/1781329717_updated_tokens.go
@@ -0,0 +1,46 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2638834880")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": "",
+			"deleteRule": "user_id = @request.auth.id",
+			"updateRule": "user_id = @request.auth.id",
+			"viewRule": "user_id = @request.auth.id"
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2638834880")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": null,
+			"deleteRule": null,
+			"updateRule": null,
+			"viewRule": null
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
